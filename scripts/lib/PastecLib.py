7e6f101bd22bb4fe3a7bda4988a9bfdcd84f2931
Florian Kräutli <florian.kraeutli@uzh.ch>
2022-01-10 16:04:28 +0100

    fix typo

--- scripts/lib/PastecLib.py
+++ scripts/lib/PastecLib.py
@@ -151,7 +151,7 @@ class PastecConnection:
         elif val == "INDEX_TAGS_NOT_WRITTEN":
             raise PastecException("Index not written.")
 
-        elif val == "IMAGE_DOWNLOADER_HTTP_ERROR"
+        elif val == "IMAGE_DOWNLOADER_HTTP_ERROR":
             raise PastecException("HTTP error when downloading an image.")
 
     def loadFileData(self, filePath):
